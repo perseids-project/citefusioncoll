bd083ac8156a2243bf2847f2ba7fe42d0ec5a653
Neel Smith <dnsmith.neel@gmail.com>
2012-09-10 07:13:05 -0400

    Clean out more unneeded stuff.

--- src/test/groovy/edu/holycross/shot/citecoll/TestCollectionSvc.groovy
+++ src/test/groovy/edu/holycross/shot/citecoll/TestCollectionSvc.groovy
@@ -10,37 +10,40 @@ class TestCollectionSvc extends GroovyTestCase {
     
    String apiKey =  System.properties['apiKey']
 
-    @Test void testSvc() {
-//        File caps = new File("testdata/unit-tests-caps.xml")
-        File caps = new File("testdata/testedit-capabilities.xml")
+    @Test void testGetObject() {
+        File caps = new File("testdata/unittests-capabilities.xml")
         assert (caps.exists())
         CollectionService svc = new CollectionService(caps, apiKey)
         assert svc
 
-
-
-        System.err.println "CONIFGURED WITH API KEY " + svc.apiKey
-        System.err.println "SVC: " + svc
-        System.err.println "configs: " + svc.citeConfig
-
-
-        // 3 collections configured in caps file
-//        int expectedSize = 2
-//        assert svc.citeConfig.keySet().size() == expectedSize
-
-//        String tstCollection = "us-states"
         String tstCollection = "greek"
-//        String tstId = "Alabama"
-       String tstId = "urn:cite:paleog:greek.op1"
-        
-        System.err.println "collection config  - " + svc.citeConfig[tstCollection]
+        String tstId = "urn:cite:paleog:greek.op1"
+
         def query = svc.getObjectQuery(tstCollection,tstId)
+
         System.err.println svc.getObjectData(tstCollection,tstId)
-
-
         System.err.println svc.getObjReply(tstId)
         
         
     }
+
+    @Test void testFirstLastReplies() {
+        assert 1 + 1 == 2
+    }
+
+
+    @Test void testPrevNextUrns() {
+        assert 1 + 1 == 2
+    }
+
+
+    @Test void testPrevNextReplies() {
+        assert 1 + 1 == 2
+    }
+
+    @Test void testCollectionSizeReply() {
+        assert 1 + 1 == 2
+    }
+
  
 }
