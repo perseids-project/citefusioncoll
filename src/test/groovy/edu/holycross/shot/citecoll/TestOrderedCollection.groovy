0772623892bf60c69b960aa3492014a21c1511dd
Neel Smith <dnsmith.neel@gmail.com>
2012-09-16 14:59:47 -0400

    Implement data retrieval for all CITE Object API requests.

---
+++ src/test/groovy/edu/holycross/shot/citecoll/TestOrderedCollection.groovy
@@ -0,0 +1,52 @@
+package edu.holycross.shot.citecoll 
+
+import edu.harvard.chs.cite.CiteUrn
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+/** Class to test cite collection service class. 
+*/
+class TestOrderedCollection extends GroovyTestCase {
+    
+   String apiKey =  System.properties['apiKey']
+    groovy.xml.Namespace citens = new groovy.xml.Namespace("http://chs.harvard.edu/xmlns/cite")
+
+    // TESTS FOR ORDERED COLLECTIONS:
+
+    @Test void testFirstLastReplies() {
+        File caps = new File("testdata/unittests-capabilities.xml")
+        assert (caps.exists())
+        CollectionService svc = new CollectionService(caps, apiKey)
+        assert svc
+        CiteUrn collUrn = new CiteUrn("urn:cite:paleog:greek")
+        // returns null on unordered collection
+
+        assert svc.getFirstObject(collUrn) == null
+        CiteUrn orderedUrn = new CiteUrn("urn:cite:hmt:msA")
+        svc.getFirstObject(orderedUrn) 
+        svc.getLastObject(orderedUrn) 
+    }
+
+    @Test void testPrevNextUrns() {
+        File caps = new File("testdata/unittests-capabilities.xml")
+        assert (caps.exists())
+        CollectionService svc = new CollectionService(caps, apiKey)
+        assert svc
+
+        CiteUrn orderedUrn = new CiteUrn("urn:cite:hmt:msA.1r")
+        System.err.println "PREV-NEXT: " + svc.getPrevNextReply(orderedUrn)
+    }
+
+    @Test void testPrevNextReplies() {
+        File caps = new File("testdata/unittests-capabilities.xml")
+        assert (caps.exists())
+        CollectionService svc = new CollectionService(caps, apiKey)
+        assert svc
+        CiteUrn orderedUrn = new CiteUrn("urn:cite:hmt:msA.1r")
+        System.err.println "NEX T OBJ => " + svc.getNextObject(orderedUrn)
+    }
+
+
+ 
+}
